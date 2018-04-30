00873948f9bf77f4ecef6fd720ceb9075221dee8
chenlingchen <171840696@smail.nju.edu.cn>
2018-04-30 13:32:10 +0800

    1

--- learngit/6e6138a3ec2b4c5d92b9a5c883c5d8b3/src/main/java/Stream.java
+++ learngit/6e6138a3ec2b4c5d92b9a5c883c5d8b3/src/main/java/Stream.java
@@ -32,43 +32,70 @@ public class Stream {
 	}
 
 	// 使用函数式范式实现
-	public static String upperString(String a)
-	{
-		if (a.length()==1)
-		{
-			return "";
-		}
-		else
-		{
-			a = a.substring(0,1).toUpperCase()+a.substring(1);
-			return a	;
-		}
-	}
+//	public static String upperString(String a)
+//	{
+//		if (a.length()==1)
+//		{
+//			return "";
+//		}
+//		else
+//		{
+//			a = a.substring(0,1).toUpperCase()+a.substring(1);
+//			return a	;
+//		}
+//	}
 	
 	
 	public static String getNamesStringFunctionally(List<String> nameList) {
-		
-		String result2 = new String("");
-		int count2 = 0;
-		for (int i=0;i<nameList.size();i++)
-		{	
-			if (upperString(nameList.get(i)).length()==1)
+//		
+//		String result2 = new String("");
+//		int count2 = 0;
+//		for (int i=0;i<nameList.size();i++)
+//		{	
+//			if (upperString(nameList.get(i)).length()==1)
+//			{
+//				continue;
+//			}
+//			else
+//			{
+//				count2 = count2 + 1;
+//				if (count2==1)
+//				{
+//					result2 = upperString(nameList.get(i)) + result2;
+//				}
+//				if (count2>1)
+//				{
+//					result2 = result2 +","+ upperString(nameList.get(i));
+//				}	
+//			}
+//		}
+//		return result2;
+//	}
+		String result1 = new String("");
+		int count1 = 0;
+		for (int i = 0; i<nameList.size(); i++) 
+		{
+			if (nameList.get(i).length() == 1)
 			{
 				continue;
 			}
 			else
 			{
-				count2 = count2 + 1;
-				if (count2==1)
+				count1 = count1 +1;
+				String a = nameList.get(i);
+				a = a.substring(0,1).toUpperCase() + a.substring(1);
+				if (count1 == 1)
 				{
-					result2 = upperString(nameList.get(i)) + result2;
+					result1 = result1 + a;
 				}
-				if (count2>1)
+				if (count1 > 1)
 				{
-					result2 = result2 +","+ upperString(nameList.get(i));
-				}	
+					result1 = result1 + "," + a;
+				}
 			}
 		}
-		return result2;
+			
+		return result1;
 	}
+
 }
