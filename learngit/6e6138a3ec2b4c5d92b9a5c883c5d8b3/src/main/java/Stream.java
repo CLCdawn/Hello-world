b6f39321455936645d49cf73214767bf6682c384
chenlingchen <171840696@smail.nju.edu.cn>
2018-04-30 12:04:25 +0800

    1

---
+++ learngit/6e6138a3ec2b4c5d92b9a5c883c5d8b3/src/main/java/Stream.java
@@ -0,0 +1,74 @@
+import java.util.List;
+
+public class Stream {
+
+	// 使用命令式范式实现
+	public static String getNamesStringImperatively(List<String> nameList) {
+		String result1 = new String("");
+		int count1 = 0;
+		for (int i = 0; i<nameList.size(); i++) 
+		{
+			if (nameList.get(i).length() == 1)
+			{
+				continue;
+			}
+			else
+			{
+				count1 = count1 +1;
+				String a = nameList.get(i);
+				a = a.substring(0,1).toUpperCase() + a.substring(1);
+				if (count1 == 1)
+				{
+					result1 = result1 + a;
+				}
+				if (count2>1)
+				{
+					result1 = result1 + "," + a;
+				}
+			}
+		}
+			
+		return result1;
+	}
+
+	// 使用函数式范式实现
+	public static String upperString(String a)
+	{
+		if (a.length()==1)
+		{
+			return "";
+		}
+		else
+		{
+			a = a.substring(0,1).toUpperCase()+a.substring(1);
+			return a	;
+		}
+	}
+	
+	
+	public static String getNamesStringFunctionally(List<String> nameList) {
+		
+		String result2 = new String("");
+		int count2 = 0;
+		for (int i=0;i<nameList.size();i++)
+		{	
+			if (upperString(nameList.get(i)).length()==1)
+			{
+				continue;
+			}
+			else
+			{
+				count2 = count2 + 1;
+				if (count2==1)
+				{
+					result2 = upperString(nameList.get(i)) + result2;
+				}
+				if (count2>1)
+				{
+					result2 = result2 +","+ upperString(nameList.get(i));
+				}	
+			}
+		}
+		return result2;
+	}
+}
